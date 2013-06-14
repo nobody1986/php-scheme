94f46a0b2d2e03a942b3534e069fd18142b3251a
snow.snow <as_star@qq.com>
2013-06-14 14:12:26 +0800

    new
    new

--- cmshead/index.php
+++ cmshead/index.php
@@ -6,6 +6,6 @@ if(!file_exists("install/install.lock")){
 */
 define('APP_NAME','Home');    		// 定义项目名称
 define('APP_PATH','./Home/');		//定义项目目录
-define('APP_DEBUG',true);			//开启调试模式
+define('APP_DEBUG',false);			//开启调试模式
 //define('HTML_PATH',APP_PATH.'Runtime/Html/');//静态缓存目录配置，默认是APP_PATH.'Html/'
 require('ThinkPHP/ThinkPHP.php');   // 加载入口文件
