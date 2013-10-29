de772156f4ff27c3b40b9993a387238597072c47
YE <NOBODY1986@163.COM>
2013-10-29 23:13:05 +0800

    minicms
    minicms

---
+++ minicms/mc-files/mc-conf.php
@@ -0,0 +1,11 @@
+<?php
+$mc_config = array (
+  'site_link' => "",
+  'site_name' => '我的网站',
+  'site_desc' => '有一个MiniCMS网站',
+  'user_name' => 'admin',
+  'user_pass' => 'admin',
+  'user_nick' => '某人',
+  'comment_code' => '',
+);
+?>
