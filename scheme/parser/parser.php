d2908c0651c8b5a4d160e46be49d3ee307ef62f4
nobody1986 <as_star@qq.com>
2016-10-28 18:35:00 +0800

    syntax parse

--- scheme/parser/parser.php
+++ scheme/parser/parser.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace \scheme\parser;
+namespace scheme\parser;
 
-class LexerException extends Exception{
+class LexerException extends \Exception{
 
 }
 
@@ -22,6 +22,7 @@ class Token{
     const Tok_MOD =12;
     const Tok_QUOTE =13;
     const Tok_BOOL = 14;
+    const Tok_BEGIN = 15;
 
     protected $_type;
     protected $_content;
@@ -29,10 +30,17 @@ class Token{
         $this->_type = $type;
         $this->_content = $c;
     }
+
+    function type(){
+        return $this->_type;
+    }
+    function content(){
+        return $this->_content;
+    }
 }
 
 class Lexer{
-    const SPLIT = "\r\n()\t\f";
+    const SPLIT = "\r\n()\t\f ";
     const SYMBOL_SPLIT = "()[]{}\",'`;#|\\";
     
 
@@ -50,7 +58,83 @@ class Lexer{
         $this->_index = 0;
     }
 
-    function tokenize(){}
+    function tokenize(){
+        $tokens = $this->split();
+        $ret  = [];
+        foreach($tokens as $token){
+            if(is_float($token)){
+                $ret []= new Token(Token::Tok_FLOAT,floatval($token));
+            }elseif(ctype_digit($token)){
+                $ret []= new Token(Token::Tok_INTEGER,intval($token));
+            }elseif($token[0] == '"'){
+                $ret []= new Token(Token::Tok_STRING,stripslashes(substr($token,1,strlen($token)-2)));
+            }elseif($token[0] == '#'&& $token[1] == '\\'){
+                $ret []= new Token(Token::Tok_CHAR,$token);
+            }elseif($token[0] == '#'&& ($token[1] == 't' || $token[1] == 'f')){
+                $ret []= new Token(Token::Tok_BOOL,$token[1] == 't');
+            }elseif($token == '\''){
+                $ret []= new Token(Token::Tok_QUOTE);
+            }
+            elseif($token == '('){
+                $ret []= new Token(Token::Tok_LP);
+            }
+            elseif($token == ')'){
+                $ret []= new Token(Token::Tok_RP);
+            }
+            elseif($token == '+'){
+                $ret []= new Token(Token::Tok_ADD);
+            }elseif($token == '-'){
+                $ret []= new Token(Token::Tok_SUB);
+            }elseif($token == '*'){
+                $ret []= new Token(Token::Tok_MUL);
+            }elseif($token == '/'){
+                $ret []= new Token(Token::Tok_DIV);
+            }elseif($token == '%'){
+                $ret []= new Token(Token::Tok_MOD);
+            }elseif(strtolower($token) == 'begin'){
+                $ret []= new Token(Token::Tok_BEGIN);
+            }else{
+                $ret []= new Token(Token::Tok_SYMBOL,$token);
+            }
+        }
+        return $ret;
+    }
+    function split(){
+        $tokens = []; 
+            for(;$this->_index < $this->_strlen;++$this->_index ){
+                if( $this->_str[$this->_index] == '\''){
+                    $tokens []= substr($this->_str, $this->_index,1);
+                }elseif(strpos(self::SPLIT,$this->_str[$this->_index])!==false){
+                    if($this->_str[$this->_index] == '('||$this->_str[$this->_index]==')'){
+                        $tokens []= substr($this->_str, $this->_index,1);
+                    }
+                }else{
+                    if($this->_str[$this->_index]=='"'){
+                        for($n=$this->_index+1;$n<$this->_strlen;++$n){
+                            if($this->_str[$n]=='\\'){
+                                ++$n;
+                                continue;
+                            }
+                            if($this->_str[$n]=='"'){
+                                $tokens []= substr($this->_str,$this->_index,$n - $this->_index+1);
+                                $this->_index  = $n;
+                                break;
+                            }
+                        }
+                    }else{
+                        for($n=$this->_index;$n<$this->_strlen;++$n){
+                            if(strpos(self::SPLIT,$this->_str[$n])!==false){
+                                $tokens []= substr($this->_str,$this->_index,$n - $this->_index);
+                                $this->_index  = $n-1;
+                                break;
+                            }
+                        }
+                    }
+                    
+                }
+            }
+            return $tokens;
+    }
 
     function next(){
         for(;$this->_index < $this->_strlen;++$this->_index ){
@@ -142,3 +226,11 @@ class Lexer{
 
 
 }
+
+$le = new Lexer();
+$le->set('(define name (lambda (x) (string-length "xj\n\"a23j"))');
+$tokens = $le->tokenize();
+include('syntax.php');
+var_dump($tokens);
+$ret = analyzeForm($tokens);
+// /var_dump($ret);
--- scheme/parser/syntax.php
+++ scheme/parser/syntax.php
@@ -1,4 +1,284 @@
 <?php
 
-namespace \scheme\parser;
+namespace scheme\parser;
 
+
+class SyntaxException extends \Exception{
+
+}
+
+
+abstract class Syntax{
+    abstract function __toString();
+}
+
+class Atom extends Syntax{
+    protected $_value;
+    function __construct($value){
+        $this->_value = $value;
+    }
+    function __toString(){
+        return $this->_value;
+    }
+}
+
+class Symbol extends Atom{
+
+}
+
+class Str extends Atom{
+    
+}
+class Integer extends Atom{
+    
+}
+
+class Double extends Atom{
+    
+}
+
+class Char extends Atom{
+    
+}
+
+class Boolean extends Atom{
+    
+}
+
+class Nil extends Atom{
+    
+}
+
+class Expr extends Syntax{
+    protected $_action;
+    protected $_args;
+    function __construct($action){
+        $this->_action = $action;
+        $this->_args = [];
+    }
+    function add($arg){
+        $this->_args []= $arg;
+    }
+    function __toString(){
+        $str ='( '.$this->_action;
+        foreach($this->_args as $arg){
+            $str .= $arg;
+        }
+        $str .=' )';
+        return $str;
+    }
+}
+
+class AddExpr extends Expr{
+    function __construct(){
+        $this->_action = '+';
+        $this->_args = [];
+    }
+}
+class SubExpr extends AddExpr{
+    protected $_action='-';
+}
+class DivExpr extends AddExpr{
+    protected $_action='/';
+}
+class MulExpr extends AddExpr{
+    protected $_action='*';
+}
+class ModExpr extends AddExpr{
+    protected $_action='%';
+}
+class QuoteExpr extends AddExpr{
+    protected $_action='quote';
+}
+
+
+class Form extends Syntax{
+    protected $_args;
+    function __construct(){
+        $this->_args = [];
+    }
+    function add($arg){
+        $this->_args []= $arg;
+    }
+    function __toString(){
+        $str ='( ';
+        foreach($this->_args as $arg){
+            $str .= $arg;
+        }
+        $str .=' )';
+        return $str;
+    }
+}
+
+
+
+function analyzeExpr($tokens,&$index = 0){
+    $size= sizeof($tokens);
+    if($index >= $size){
+        return null;
+    }
+    switch($tokens[$index]->type()){
+            case Token::Tok_SYMBOL:
+                $ret = new Expr($tokens[$index]->content());
+                ++$index;
+                break;
+            case Token::Tok_LP:
+                if($tokens[$index+1]->type() == Token::Tok_BEGIN){
+                    $index +=2;
+                    $item = analyzeForm($tokens ,$index);
+                }else{
+                    ++$index;
+                    $item = analyzeExpr($tokens ,$index);
+                }
+                $ret = new Expr($item);
+                break;
+            case Token::Tok_RP:
+                ++$index;
+                return $ret;
+                break;
+            case Token::Tok_ADD:
+                $item = AddExpr();
+                ++$index;
+                break;
+            case Token::Tok_SUB:
+                $item = SubExpr();
+                ++$index;
+                break;
+            case Token::Tok_DIV:
+                $item = DivExpr();
+                ++$index;
+                break;
+            case Token::Tok_MUL:
+                $item = MulExpr();
+                ++$index;
+                break;
+            case Token::Tok_MOD:
+                $item = ModExpr();
+                ++$index;
+                break;
+            case Token::Tok_QUOTE:
+                $item = QuoteExpr();
+                ++$index;
+                break;
+            
+        }
+    for($i = $index;$i<$size;++$i){
+        switch($tokens[$index]->type()){
+            case Token::Tok_SYMBOL:
+                $item = new Symbol($tokens[$index]->content());
+                break;
+            case Token::Tok_STRING:
+                $item = new Str($tokens[$index]->content());
+                break;
+            case Token::Tok_INTEGER:
+                $item = new Integer($tokens[$index]->content());
+                break;
+            case Token::Tok_FLOAT:
+                $item = new Double($tokens[$index]->content());
+                break;
+            case Token::Tok_CHAR:
+                $item = new Char($tokens[$index]->content());
+                break;
+            case Token::Tok_LP:
+                $item = analyzeExpr($tokens ,$index);
+                if($tokens[$index+1]->type() == Token::Tok_BEGIN){
+                    $index +=2;
+                    $item = analyzeForm($tokens ,$index);
+                }
+                break;
+            case Token::Tok_RP:
+                ++$index;
+                return $ret;
+                break;
+            case Token::Tok_ADD:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_SUB:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_DIV:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_MUL:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_MOD:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_QUOTE:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_BOOL:
+                $item = new Boolean($tokens[$index]->content());
+                break;
+            
+        }
+        $ret->add($item);
+    }
+    return $ret;
+}
+//function analyzeAtom($tokens,&$index = 0){}
+
+function analyzeForm($tokens,&$index = 0){
+    $size= sizeof($tokens);
+    if($index >= $size){
+        return null;
+    }
+    $ret =  new Form();
+    for(;$index<$size;++$index){
+        switch($tokens[$index]->type()){
+            case Token::Tok_SYMBOL:
+                $item = new Symbol($tokens[$index]->content());
+                break;
+            case Token::Tok_STRING:
+                $item = new Str($tokens[$index]->content());
+                break;
+            case Token::Tok_INTEGER:
+                $item = new Integer($tokens[$index]->content());
+                break;
+            case Token::Tok_FLOAT:
+                $item = new Double($tokens[$index]->content());
+                break;
+            case Token::Tok_CHAR:
+                $item = new Char($tokens[$index]->content());
+                break;
+            case Token::Tok_LP:
+                $item = analyzeExpr($tokens ,$index);
+                if($tokens[$index+1]->type() == Token::Tok_BEGIN){
+                    $index += 2;
+                    $item = analyzeForm($tokens ,$index);
+                }
+                break;
+            case Token::Tok_RP:
+                ++$index ;
+                return $ret;
+                break;
+            case Token::Tok_ADD:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_SUB:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_DIV:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_MUL:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_MOD:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_QUOTE:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+            case Token::Tok_BOOL:
+                $item = analyzeExpr($tokens ,$index);
+                break;
+
+            
+        }
+        $ret->add($item);
+    }
+    return $ret;
+        
+}
